8eea743b9be04ea054f3a87d794df6a7761a9663
G-Carneiro <carneiro.gabr@gmail.com>
2022-02-13 10:45:13 -0300

    diffuser and grover operator

--- grover.py
+++ grover.py
@@ -1,26 +1,29 @@
 from typing import Union, List
 from math import sqrt, acos, pi
 
-from qiskit import QuantumCircuit, QuantumRegister, ClassicalRegister
+from qiskit import QuantumCircuit, QuantumRegister, ClassicalRegister, execute
+from qiskit.circuit.gate import Gate
 from qiskit.circuit.quantumregister import Qubit
-from qiskit.circuit.library import MCMT, ZGate
 from qiskit.providers.aer.backends.aer_simulator import AerSimulator
 
 
-def phase_oracle(qc: QuantumCircuit,
-                 control_qubits: Union[QuantumRegister, List[int]],
-                 work_qubits: Union[QuantumRegister, List[int]],
-                 target_qubit: Union[QuantumRegister, int],
-                 state: str
-                 ) -> None:
+def phase_oracle(state: str) -> Gate:
+    qc: QuantumCircuit = QuantumCircuit(len(state))
     for i in range(len(state[:-1])):
         if (state[i] == "0"):
-            qc.x(control_qubits[i])
+            qc.x(qc.qubits[i])
 
     if (state[-1] == "0"):
-        qc.x(target_qubit)
+        qc.x(qc.qubits[-1])
 
-    multiple_control_gate(qc, control_qubits, work_qubits, target_qubit)
+    qc.h(qc.qubits[-1])
+    qc.mcx(qc.qubits[:-1], qc.qubits[-1])
+    qc.h(qc.qubits[-1])
+
+    gate: Gate = qc.to_gate()
+    gate.name = "Oracle"
+
+    return gate
 
 
 def multiple_control_gate(qc: QuantumCircuit,
@@ -50,86 +53,61 @@ def multiple_control_gate(qc: QuantumCircuit,
 
 
 def grover() -> None:
-    entries: int = 4
+    num_qubits: int = 4
 
-    control: QuantumRegister = QuantumRegister(entries - 1, "ctrl")
-    work: QuantumRegister = QuantumRegister(entries - 2, "work")
-    target: QuantumRegister = QuantumRegister(1, "target")
-    c_bits: ClassicalRegister = ClassicalRegister(entries)
-    circuit: QuantumCircuit = QuantumCircuit(control, work, target, c_bits)
+    circuit: QuantumCircuit = QuantumCircuit(num_qubits)
 
-    entries = 2**entries
+    entries = 2**num_qubits
     steps: int = round(acos(1 / sqrt(entries)) / acos((entries - 2) / entries))
-    # print(steps)
     # steps = int((pi/4)*sqrt(entries/1))
-    # print(steps)
 
-    apply_hadamard_gate(qc=circuit, qubits=control)
-    apply_hadamard_gate(qc=circuit, qubits=target)
+    circuit.h(circuit.qubits)
 
     for _ in range(steps):
-        circuit.barrier()
-        phase_oracle(qc=circuit, control_qubits=control,
-                     work_qubits=work, target_qubit=target, state="1111")
-        circuit.barrier()
-        apply_hadamard_gate(qc=circuit, qubits=control)
-        apply_hadamard_gate(qc=circuit, qubits=target)
+        # circuit.barrier()
+        # phase_oracle(state="1111")
+        # circuit.barrier()
+        # circuit.append(grover_diffuser(num_qubits), circuit.qubits)
+        circuit.append(grover_operator("1111"), circuit.qubits)
 
-        circuit.barrier()
-        apply_z_gate(qc=circuit, qubits=control)
-        apply_z_gate(qc=circuit, qubits=target)
-
-        apply_hadamard_gate(qc=circuit, qubits=control)
-        apply_hadamard_gate(qc=circuit, qubits=target)
-
-    circuit.measure([control[i] for i in range(len(control))] + [target[0]],
-                    [c_bits[i] for i in range(len(c_bits))])
+    circuit.measure_all()
+    print(circuit)
 
     sim = AerSimulator()
-    counts = sim.run(circuit).result().get_counts()
+    counts = execute(circuit, sim).result().get_counts()
 
-    print(circuit)
     print(counts)
 
 
-def apply_hadamard_gate(qc: QuantumCircuit,
-                        qubits: Union[QuantumRegister, List[int]]
-                        ) -> None:
-    for qubit in qubits:
-        qc.h(qubit)
+def grover_diffuser(size: int) -> Gate:
+    qc: QuantumCircuit = QuantumCircuit(size)
 
-    return None
+    qc.h(qc.qubits)
+    qc.x(qc.qubits)
+
+    # Make a multi controlled z gate
+    qc.h(size - 1)
+    qc.mcx(qc.qubits[:-1], size - 1)
+    qc.h(size - 1)
+
+    qc.x(qc.qubits)
+    qc.h(qc.qubits)
+
+    gate: Gate = qc.to_gate()
+    gate.name = "Diffuser"
+    return gate
 
 
-def apply_z_gate(qc: QuantumCircuit,
-                 qubits: Union[QuantumRegister, List[int]]
-                 ) -> None:
-    for qubit in qubits:
-        qc.z(qubit)
+def grover_operator(state: str) -> Gate:
+    qc: QuantumCircuit = QuantumCircuit(len(state))
+    qc.append(phase_oracle(state), qc.qubits)
+    qc.append(grover_diffuser(len(state)), qc.qubits)
 
-    return None
+    gate: Gate = qc.to_gate()
+    gate.name = "G"
+
+    return gate
 
 
 grover()
-
-# qc = QuantumCircuit(2)
-# qc.h(0)
-# qc.h(1)
-#
-# qc.cz(0, 1)
-# qc.h(0)
-# qc.h(1)
-#
-# qc.z(0)
-# qc.z(1)
-# qc.cz(0, 1)
-#
-# qc.h(0)
-# qc.h(1)
-#
-# qc.measure_all()
-#
-# sim = AerSimulator()
-# counts = sim.run(qc).result().get_counts()
-# print(counts)
 
