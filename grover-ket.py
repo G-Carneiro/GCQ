a378554037379f10d81cbb6b590c11c21e64c25d
G-Carneiro <carneiro.gabr@gmail.com>
2022-02-21 13:32:13 -0300

    grover with ket

---
+++ grover-ket.py
@@ -0,0 +1,61 @@
+from math import acos, sqrt
+from typing import List
+
+from ket import *
+
+
+def phase_oracle(qubits: quant, state: str):
+    flip_qubits: List = []
+    # state = state[::-1]
+    for i in range(len(state)):
+        if (state[i] == "0"):
+            flip_qubits.append(qubits[i])
+
+    if flip_qubits:
+        X(flip_qubits)
+    ctrl(qubits[:-1], Z, qubits[-1])
+    if flip_qubits:
+        X(flip_qubits)
+
+    return None
+
+
+def grover_diffuser(qubits: quant):
+    H(qubits)
+    X(qubits)
+
+    ctrl(qubits[:-1], Z, qubits[-1])
+
+    X(qubits)
+    H(qubits)
+
+    return None
+
+
+def grover_operator(qubits: quant, states: List[str]):
+    for state in states:
+        phase_oracle(qubits, state)
+
+    grover_diffuser(qubits)
+
+    return None
+
+
+def grover(states: List[str]) -> None:
+    num_qubits: int = len(states[0])
+
+    qubits: quant = quant(num_qubits)
+    entries = 2**num_qubits
+    steps: int = int(acos(1 / sqrt(entries)) / acos((entries - 2) / entries))
+    # steps = int((pi/4)*sqrt(entries/1))
+    H(qubits)
+
+    for _ in range(steps):
+        grover_operator(qubits, states)
+
+    print(dump(qubits).show())
+
+    return None
+
+
+grover(["110", "100"])
